6cceb12c76336f9488861f1bf5e5de456191d8ad
Vladimir Drayling <>
2025-01-18 23:45:04 +0100

    chore(docker): change

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import Elysia from 'elysia';
 import cors from '@elysiajs/cors';
 
-new Elysia({ prefix: 'api' })
+new Elysia()
   .use(cors())
   .get('/elysia-version', () => ({ version: '1.2.10' }))
   .listen(process.env['PORT'], ({ url }) => console.log(`🦊 ${url.href}`));
